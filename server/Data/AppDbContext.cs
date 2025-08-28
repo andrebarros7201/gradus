e234628fdfcee5849290e52337dedc9522933f4e
Andre Barros <andrebarros7201@gmail.com>
2025-08-28 22:22:07 +0100

    Add `AppDbContext` to configure entity relationships and database model setup.

---
+++ server/Data/AppDbContext.cs
@@ -0,0 +1,91 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Models;
+
+namespace Server.Data;
+
+public class AppDbContext : DbContext {
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Class> Classes { get; set; }
+    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<Professor> Professors { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+    public DbSet<ClassSubject> ClassSubjects { get; set; }
+    public DbSet<ClassStudents> ClassStudents { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+
+        // Professor - Subject
+        modelBuilder.Entity<Professor>()
+            .HasMany(p => p.Subjects)
+            .WithOne(s => s.Professor)
+            .HasForeignKey(s => s.ProfessorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Class - Subject
+        modelBuilder.Entity<ClassSubject>()
+            .HasKey(cs => new { cs.ClassId, cs.SubjectId });
+
+        modelBuilder.Entity<ClassSubject>()
+            .HasOne(cs => cs.Class)
+            .WithMany(c => c.Subjects)
+            .HasForeignKey(cs => cs.ClassId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<ClassSubject>()
+            .HasOne(cs => cs.Subject)
+            .WithMany(s => s.Classes)
+            .HasForeignKey(cs => cs.SubjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Class - User
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Class)
+            .WithOne(c => c.User)
+            .HasForeignKey<User>(u => u.ClassId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Professor - User
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Professor)
+            .WithOne(p => p.User)
+            .HasForeignKey<User>(u => u.ProfessorId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Class - Student
+        modelBuilder.Entity<ClassStudents>()
+            .HasKey(cs => new { cs.ClassId, cs.StudentId });
+
+        modelBuilder.Entity<ClassStudents>()
+            .HasOne(cs => cs.Class)
+            .WithMany(c => c.Students)
+            .HasForeignKey(cs => cs.ClassId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<ClassStudents>()
+            .HasOne(cs => cs.Student)
+            .WithMany(s => s.Classes)
+            .HasForeignKey(cs => cs.StudentId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Grade - Student
+        modelBuilder.Entity<Grade>()
+            .HasOne(g => g.Student)
+            .WithMany(s => s.Grades)
+            .HasForeignKey(g => g.StudentId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Grade - Subject
+        modelBuilder.Entity<Grade>()
+            .HasOne(g => g.Subject)
+            .WithMany(s => s.Grades)
+            .HasForeignKey(g => g.SubjectId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+
+}
