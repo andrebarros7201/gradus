74b135303fd7fc48abc2c0514b3d2adc99177418
Andre Barros <andrebarros7201@gmail.com>
2025-08-29 11:17:45 +0100

    Add `Role` property to `Class` and `Professor` entities; include timestamps in `Professor` model

--- server/Models/Class.cs
+++ server/Models/Class.cs
@@ -6,6 +6,7 @@ public class Class {
     public string UserName { get; set; }
     public string Password { get; set; }
     public string SchoolYear { get; set; }
+    public Role Role { get; set; } = Role.Class;
     public List<ClassStudents> Students { get; set; }
     public List<ClassSubject> Subjects { get; set; }
     public bool IsActive { get; set; }
--- server/Models/Professor.cs
+++ server/Models/Professor.cs
@@ -6,4 +6,7 @@ public class Professor {
     public string UserName { get; set; }
     public string Password { get; set; }
     public List<Subject> Subjects { get; set; }
+    public Role Role { get; set; } = Role.Professor;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; } = null;
 }
