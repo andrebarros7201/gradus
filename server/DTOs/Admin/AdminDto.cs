41b5648a211988d56967ba8c890806ebf224f1f5
Andre Barros <andrebarros7201@gmail.com>
2025-08-29 15:18:53 +0100

    Set default values for `Name` and `Username` properties in `AdminDto` to avoid null reference issues.

--- server/DTOs/Admin/AdminDto.cs
+++ server/DTOs/Admin/AdminDto.cs
@@ -2,6 +2,6 @@ namespace Server.DTOs.Admin;
 
 public class AdminDto {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Username { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
 }
