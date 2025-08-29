622cbe55a1a47eae4d00f729b4e6892686878889
Andre Barros <andrebarros7201@gmail.com>
2025-08-29 15:17:53 +0100

    Add DTOs for Admin registration, login, and data transfer

---
+++ server/DTOs/Admin/AdminDto.cs
@@ -0,0 +1,7 @@
+namespace Server.DTOs.Admin;
+
+public class AdminDto {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Username { get; set; }
+}
---
+++ server/DTOs/Admin/AdminLoginDto.cs
@@ -0,0 +1,6 @@
+namespace Server.DTOs.Admin;
+
+public class AdminLoginDto {
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
