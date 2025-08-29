2d46e1a872ba3ab942993b41e3e8fb5ac10f584f
Andre Barros <andrebarros7201@gmail.com>
2025-08-29 20:41:10 +0100

    Implement `Login` method in `AdminService` with admin credential validation, including username existence and password verification.

--- server/Services/AdminService.cs
+++ server/Services/AdminService.cs
@@ -1,6 +1,7 @@
 using Server.DTOs.Admin;
 using Server.Interfaces.Admin;
 using Server.Interfaces.Repositories;
+using Server.Models;
 using Server.Results;
 
 namespace Server.Services;
@@ -12,8 +13,26 @@ public class AdminService : IAdminService {
         _adminRepository = adminRepository;
     }
 
-    public Task<ServiceResult<AdminDto>> Login(AdminLoginDto dto) {
-        throw new NotImplementedException();
+    public async Task<ServiceResult<AdminDto>> Login(AdminLoginDto dto) {
+        var user = await _adminRepository.GetAdminByUsername(dto.Username);
+
+        // User not found
+        if (user == null) {
+            return ServiceResult<AdminDto>.Error(ServiceResultStatus.NotFound, "Admin not found!");
+        }
+
+        // Check password
+        bool isPasswordValid = BCrypt.Net.BCrypt.Verify(dto.Password, user.Password);
+
+        if (!isPasswordValid) {
+            return ServiceResult<AdminDto>.Error(ServiceResultStatus.Unauthorized, "Invalid credentials!");
+        }
+
+        return ServiceResult<AdminDto>.Success(new AdminDto {
+            Id = user.Id,
+            Name = user.Name,
+            Username = user.Username
+        });
     }
 
     public async Task<ServiceResult<bool>> Register(AdminRegisterDto registerDto) {
@@ -21,7 +40,7 @@ public class AdminService : IAdminService {
 
         // Username already exists
         if (admin != null) {
-            return ServiceResult<bool>.Error(ServiceResultStatus.Conflict, "Username already exists");
+            return ServiceResult<bool>.Error(ServiceResultStatus.Conflict, "Username already exists!");
         }
 
         var newAdmin = new Models.Admin {
