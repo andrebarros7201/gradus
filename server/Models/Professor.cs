20ac1a7e4648bff4aee83510aca992d958096357
Andre Barros <andrebarros7201@gmail.com>
2025-08-29 17:53:24 +0100

    Update `UserName` property to `Username` in `Professor` and `Admin` models to maintain naming consistency

--- server/Models/Professor.cs
+++ server/Models/Professor.cs
@@ -3,7 +3,7 @@ namespace Server.Models;
 public class Professor {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string UserName { get; set; }
+    public string Username { get; set; }
     public string Password { get; set; }
     public List<Subject> Subjects { get; set; }
     public Role Role { get; set; } = Role.Professor;
