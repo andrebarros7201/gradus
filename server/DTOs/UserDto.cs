bc3c1a17ceac42a2117a742a66f51573af199b3c
Andre Barros <andrebarros7201@gmail.com>
2025-08-30 15:04:19 +0100

    Rename `UserAuthDto` to `UserDto` and add `Name` property with data annotations.

--- server/DTOs/UserDto.cs
+++ server/DTOs/UserDto.cs
@@ -1,9 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Server.Models;
 
 namespace Server.DTOs;
 
-public class UserAuthDto {
+public class UserDto {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public Role Role { get; set; }
 }
