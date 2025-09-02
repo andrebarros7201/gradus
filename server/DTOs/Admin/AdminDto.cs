4652933dc684597cb39d26da8ef0d22f5dfceec7
Andre Barros <andrebarros7201@gmail.com>
2025-09-02 21:10:37 +0100

    Remove unused `Username` property from `AdminDto` to simplify DTO structure.

--- server/DTOs/Admin/AdminDto.cs
+++ server/DTOs/Admin/AdminDto.cs
@@ -3,5 +3,4 @@ namespace Server.DTOs.Admin;
 public class AdminDto {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
 }
