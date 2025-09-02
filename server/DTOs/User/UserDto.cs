1ade09efc8b18564573bdf8ca3ec293876cf1168
Andre Barros <andrebarros7201@gmail.com>
2025-09-02 21:10:45 +0100

    Add `AdminDto` property to `UserDto` to represent admin-specific details.

--- server/DTOs/User/UserDto.cs
+++ server/DTOs/User/UserDto.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Server.DTOs.Admin;
 using Server.Models;
 
 namespace Server.DTOs;
@@ -6,6 +7,6 @@ namespace Server.DTOs;
 public class UserDto {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
     public Role Role { get; set; }
+    public AdminDto? Admin { get; set; }
 }
