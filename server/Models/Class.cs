c9952286cf0ce0d3981e607d860a5b455441a825
Andre Barros <andrebarros7201@gmail.com>
2025-10-06 15:50:03 +0100

    Initialize properties in `Class` model with default values:
    - Set default values for `Name`, `SchoolYear`, and `IsActive` properties.
    - Initialize `Students` and `Subjects` lists to empty collections.

--- server/Models/Class.cs
+++ server/Models/Class.cs
@@ -2,13 +2,13 @@ namespace Server.Models;
 
 public class Class {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string SchoolYear { get; set; }
-    public bool IsActive { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string SchoolYear { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
 
     public User User { get; set; }
     public int UserId { get; set; }
-    
-    public List<ClassStudents> Students { get; set; }
-    public List<ClassSubject> Subjects { get; set; }
+
+    public List<ClassStudents> Students { get; set; } = [];
+    public List<ClassSubject> Subjects { get; set; } = [];
 }
