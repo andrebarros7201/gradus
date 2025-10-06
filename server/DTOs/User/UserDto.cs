02e77991e74d3dbe225e5e5919f43510bd5e05b4
Andre Barros <andrebarros7201@gmail.com>
2025-10-06 16:47:03 +0100

    Add `JsonIgnore` attributes to `Admin` and `Class` properties in `UserDto`
    - Prevent serialization of `Admin` and `Class` properties when their values are `null`.
    - Import `System.Text.Json.Serialization` and `Server.DTOs.Class` for the updates.

--- server/DTOs/User/UserDto.cs
+++ server/DTOs/User/UserDto.cs
@@ -1,5 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using Server.DTOs.Admin;
+using Server.DTOs.Class;
 using Server.Models;
 
 namespace Server.DTOs;
@@ -8,5 +10,10 @@ public class UserDto {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public Role Role { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public AdminDto? Admin { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public ClassDto? Class { get; set; } 
 }
