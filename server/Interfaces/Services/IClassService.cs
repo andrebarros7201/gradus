d0ae873d6c9c459f6a56454faa21c2cfca9cf499
Andre Barros <andrebarros7201@gmail.com>
2025-10-07 17:57:02 +0100

    Remove `ClassController`, `ClassService`, and `IClassService`
    - Deleted `ClassController`, `ClassService`, and `IClassService` as part of redundancy cleanup.
    - Simplifies codebase by removing unused class-related logic and dependencies.

--- server/Interfaces/Services/IClassService.cs
+++
@@ -1,8 +0,0 @@
-using Server.DTOs.Class;
-using Server.Results;
-
-namespace Server.Interfaces.Services;
-
-public interface IClassService {
-    public Task<ServiceResult<bool>> Create(ClassRegisterDto dto, int userId);
-}
