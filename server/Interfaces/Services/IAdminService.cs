78f5d96c3059d7fc6662d175928193dafc11ae3f
Andre Barros <andrebarros7201@gmail.com>
2025-10-07 17:56:15 +0100

    Remove `AdminController`, `AdminService`, and `IAdminService`
    - Deleted `AdminController`, `AdminService`, and `IAdminService` due to redundancy and disuse.
    - Simplifies codebase by eliminating unused admin-related logic and dependencies.

--- server/Interfaces/Services/IAdminService.cs
+++
@@ -1,9 +0,0 @@
-using Server.DTOs;
-using Server.DTOs.Admin;
-using Server.Results;
-
-namespace Server.Interfaces.Admin;
-
-public interface IAdminService {
-    public Task<ServiceResult<bool>> Register(AdminRegisterDto registerDto);
-}
