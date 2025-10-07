2e43bd2756d533ae2b348401c6656513af83f64f
Andre Barros <andrebarros7201@gmail.com>
2025-10-07 15:34:49 +0100

    Initialize `Subjects` property in `Professor` model with an empty list
    - Updated `Subjects` property in `Professor` model to default to an empty list (`[]`).
    - Ensures proper initialization, preventing potential null reference issues.

--- server/Models/Professor.cs
+++ server/Models/Professor.cs
@@ -3,7 +3,7 @@ namespace Server.Models;
 public class Professor {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<Subject> Subjects { get; set; }
+    public List<Subject> Subjects { get; set; } = [];
 
     public User User { get; set; }
 }
