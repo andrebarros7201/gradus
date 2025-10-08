013e0e31a35f05ca81f40a5da9c7ff29a815d763
Andre Barros <andrebarros7201@gmail.com>
2025-10-08 11:44:39 +0100

    Make `userId` optional in `IUserService.Create` method signature

--- server/Interfaces/Services/IUserService.cs
+++ server/Interfaces/Services/IUserService.cs
@@ -1,8 +1,9 @@
 using Server.DTOs;
+using Server.DTOs.User;
 using Server.Results;
 
 namespace Server.Interfaces.Services;
 
 public interface IUserService {
-    public Task<ServiceResult<bool>> Create(UserCreateDto dto, int userId);
+    public Task<ServiceResult<bool>> Create(UserCreateDto dto, int? userId);
 }
