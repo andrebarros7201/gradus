8a29536224dc7a97e5de4130b755871dd4479cbd
Andre Barros <andrebarros7201@gmail.com>
2025-10-14 21:59:23 +0100

    Add `Name` property to `ClassDto` for class identification

--- server/DTOs/Class/ClassDto.cs
+++ server/DTOs/Class/ClassDto.cs
@@ -2,6 +2,7 @@ namespace Server.DTOs.Class;
 
 public class ClassDto {
     public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string SchoolYear { get; set; } = string.Empty;
     public bool IsActive { get; set; } = true;
 }
