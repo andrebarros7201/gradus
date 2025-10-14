9ab3022938072a0e047ab65fa170ee691814f6e7
Andre Barros <andrebarros7201@gmail.com>
2025-10-14 13:59:50 +0100

    Add comment to clarify the Many to Many relation between students and classes

--- server/Models/Student.cs
+++ server/Models/Student.cs
@@ -3,6 +3,9 @@ namespace Server.Models;
 public class Student {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    // Many-to-many: a student can be enrolled in multiple classes (current and past)
     public List<ClassStudents> Classes { get; set; }
+
     public List<Grade> Grades { get; set; }
 }
