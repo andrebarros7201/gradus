04a7645c58e6c26dd27151cabaf1ae9e5ceca226
Andre Barros <andrebarros7201@gmail.com>
2025-10-14 18:45:02 +0100

    Remove `ClassStudents` and `ClassSubject` models
    - Deleted unused `ClassStudents` and `ClassSubject` models to clean up the codebase.

--- server/Models/ClassStudents.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Models;
-
-public class ClassStudents {
-    public int StudentId { get; set; }
-    public Student Student { get; set; }
-
-    public int ClassId { get; set; }
-    public Class Class { get; set; }
-}
--- server/Models/ClassSubject.cs
+++
@@ -1,9 +0,0 @@
-namespace Server.Models;
-
-public class ClassSubject {
-    public int ClassId { get; set; }
-    public Class Class { get; set; }
-
-    public int SubjectId { get; set; }
-    public Subject Subject { get; set; }
-}
