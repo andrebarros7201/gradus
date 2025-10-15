73123bcc87d60b2d87ab5c2e0f6ef9c8ba024db2
Andre Barros <andrebarros7201@gmail.com>
2025-10-15 11:54:45 +0100

    Hash user passwords securely using bcrypt in `Configuration` class
    - Added `SECRET_USER_PASSWORD` property that hashes the environment variable `SECRET_USER_PASSWORD` using `BCrypt`.

--- server/Configuration.cs
+++ server/Configuration.cs
@@ -2,4 +2,7 @@ namespace Server;
 
 static class Configuration {
     internal static string JWT_SECRET { get; } = Environment.GetEnvironmentVariable("JWT_SECRET");
+
+    internal static string SECRET_USER_PASSWORD { get; } =
+        BCrypt.Net.BCrypt.HashPassword(Environment.GetEnvironmentVariable("SECRET_USER_PASSWORD"));
 }
