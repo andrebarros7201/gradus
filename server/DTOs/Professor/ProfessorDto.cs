7065d22d6b58acd904094afd5f83cd5c0fc986c7
Andre Barros <andrebarros7201@gmail.com>
2025-10-19 20:07:00 +0100

    Refactor `ProfessorDto` to use `SubjectSimpleDto` for streamlined subject data handling

--- server/DTOs/Professor/ProfessorDto.cs
+++ server/DTOs/Professor/ProfessorDto.cs
@@ -1,8 +1,9 @@
+using Server.DTOs.Subject;
 using Server.Models;
 
 namespace Server.DTOs.Professor;
 
 public class ProfessorDto {
     public int Id { get; set; }
-    public List<Subject> Subjects { get; set; } = [];
+    public List<SubjectSimpleDto> Subjects { get; set; } = [];
 }
