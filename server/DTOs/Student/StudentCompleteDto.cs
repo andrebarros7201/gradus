43e52463764c3c4e1c3428e1d1a60a72591ad2c4
Andre Barros <andrebarros7201@gmail.com>
2025-10-19 20:06:04 +0100

    Refactor `StudentDto` into `StudentCompleteDto` and `StudentSimpleDto`
    - Removed `StudentDto` and introduced `StudentCompleteDto` and `StudentSimpleDto` to streamline student-related data handling.
    - Added `ClassId` property to `StudentRegisterDto` for enhanced registration capabilities.
    - Temporarily commented out `Grades` property in `StudentCompleteDto` with a note to replace it with a `GradeDto` in the future.

--- server/DTOs/Student/StudentCompleteDto.cs
+++ server/DTOs/Student/StudentCompleteDto.cs
@@ -2,9 +2,11 @@ using Server.Models;
 
 namespace Server.DTOs.Student;
 
-public class StudentDto {
+public class StudentCompleteDto {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
-    public List<Grade> Grades { get; set; } = [];
+
+    // TODO create a GradeDto
+    // public List<Grade> Grades { get; set; } = [];
     public int ClassId { get; set; }
 }
--- server/DTOs/Student/StudentRegisterDto.cs
+++ server/DTOs/Student/StudentRegisterDto.cs
@@ -2,4 +2,5 @@ namespace Server.DTOs.Student;
 
 public class StudentRegisterDto {
     public string Name { get; set; } = string.Empty;
+    public int ClassId { get; set; }
 }
