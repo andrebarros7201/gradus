010eb69d2d1107fc6598646098cce9e69006ecf0
Andre Barros <andrebarros7201@gmail.com>
2025-10-21 22:21:49 +0100

    Update `GetStudentById` method return type in `IStudentRepository`
    - Changed the return type of `GetStudentById` to `Task<Student?>` to handle potential null values and enhance nullability safety.
    - Simplified namespace declaration by adding a `using` directive for `Server.Models`.

--- server/Interfaces/Repositories/IStudentRepository.cs
+++ server/Interfaces/Repositories/IStudentRepository.cs
@@ -1,7 +1,9 @@
+using Server.Models;
+
 namespace Server.Interfaces.Repositories;
 
 public interface IStudentRepository {
-    public Task<Models.Student> GetStudentById(int id);
+    public Task<Student?> GetStudentById(int id);
     public Task CreateStudent(Models.Student student);
     public Task DeleteStudent(Models.Student student);
     public Task UpdateStudent(Models.Student student);
