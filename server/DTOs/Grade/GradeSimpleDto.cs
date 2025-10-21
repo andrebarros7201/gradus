68acc9242c64a49d0ffafd261f2ee75f8883ac2e
Andre Barros <andrebarros7201@gmail.com>
2025-10-21 22:22:21 +0100

    Update `GradeSimpleDto` to include `SubjectName` and configure JSON serialization
    - Added `SubjectName` property to improve grade-related data representation.
    - Applied `[JsonIgnore]` attribute to `StudentId` for conditional serialization based on default value.

--- server/DTOs/Grade/GradeSimpleDto.cs
+++ server/DTOs/Grade/GradeSimpleDto.cs
@@ -1,8 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace Server.DTOs.Grade;
 
 public class GradeSimpleDto {
     public int Id { get; set; }
     public int Value { get; set; }
-    public int SubjectId { get; set; }
+    public string SubjectName { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public int StudentId { get; set; }
+    
 }
