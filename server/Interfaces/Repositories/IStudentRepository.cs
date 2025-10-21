cd71bba2de20fff040290090d220b78e01f83052
Andre Barros <andrebarros7201@gmail.com>
2025-10-21 22:01:49 +0100

    Remove unused `GetStudentByUsername` method from `IStudentRepository`
    - Removed the `GetStudentByUsername` method as it is no longer being utilized in the application logic.

--- server/Interfaces/Repositories/IStudentRepository.cs
+++ server/Interfaces/Repositories/IStudentRepository.cs
@@ -2,7 +2,6 @@ namespace Server.Interfaces.Repositories;
 
 public interface IStudentRepository {
     public Task<Models.Student> GetStudentById(int id);
-    public Task<Models.Student> GetStudentByUsername(string username);
     public Task CreateStudent(Models.Student student);
     public Task DeleteStudent(Models.Student student);
     public Task UpdateStudent(Models.Student student);
