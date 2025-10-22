89c8652a12903e55b2b8ba2de06b6d41cc630bdd
Andre Barros <andrebarros7201@gmail.com>
2025-10-22 17:23:06 +0100

    Update `IStudentService` to add `UpdateStudent` method and remove unused methods
    - Added `UpdateStudent` method with parameters for updating a student's information.
    - Removed unused methods `FetchAllStudents` and `FetchStudentByUsername` for better interface clarity.
    - Cleaned up outdated comments to improve readability.

--- server/Interfaces/Services/IStudentService.cs
+++ server/Interfaces/Services/IStudentService.cs
@@ -4,13 +4,8 @@ using Server.Results;
 namespace Server.Interfaces.Services;
 
 public interface IStudentService {
-    public Task<ServiceResult<StudentCompleteDto>> FetchAllStudents(int classId);
     public Task<ServiceResult<StudentCompleteDto>> FetchStudentById(int id);
-    public Task<ServiceResult<StudentCompleteDto>> FetchStudentByUsername(string username);
     public Task<ServiceResult<bool>> CreateStudent(int currentUserId, StudentRegisterDto dto);
-
+    public Task<ServiceResult<StudentCompleteDto>> UpdateStudent(int currentUserId, int studentId, StudentUpdateDto dto);
     public Task<ServiceResult<bool>> DeleteStudent(int currentUserId, int studentId);
-
-    // TODO add update student args
-    public Task<ServiceResult<StudentCompleteDto>> UpdateStudent();
 }
