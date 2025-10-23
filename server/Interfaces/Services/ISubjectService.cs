8aa469b234ceb408abb2003d15a429b8e197a4cd
Andre Barros <andrebarros7201@gmail.com>
2025-10-23 17:27:26 +0100

    Update `ISubjectService` interface to use `SubjectCompleteDto`
    - Changed return types of `GetSubjectById`, `CreateSubject`, and `UpdateSubject` methods from `SubjectSimpleDto` to `SubjectCompleteDto` for more detailed subject representations.

--- server/Interfaces/Services/ISubjectService.cs
+++ server/Interfaces/Services/ISubjectService.cs
@@ -4,8 +4,8 @@ using Server.Results;
 namespace Server.Interfaces.Services;
 
 public interface ISubjectService {
-    public Task<ServiceResult<SubjectSimpleDto>> GetSubjectById(int currentUserId, int subjectId);
-    public Task<ServiceResult<SubjectSimpleDto>> CreateSubject(int currentUserId, SubjectCreateDto dto);
-    public Task<ServiceResult<SubjectSimpleDto>> UpdateSubject(int currentUserId, SubjectUpdateDto dto);
+    public Task<ServiceResult<SubjectCompleteDto>> GetSubjectById(int currentUserId, int subjectId);
+    public Task<ServiceResult<SubjectCompleteDto>> CreateSubject(int currentUserId, SubjectCreateDto dto);
+    public Task<ServiceResult<SubjectCompleteDto>> UpdateSubject(int currentUserId, SubjectUpdateDto dto);
     public Task<ServiceResult<bool>> DeleteSubject(int currentUserId, int subjectId);
 }
