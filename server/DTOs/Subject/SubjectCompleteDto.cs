145d942862b749ef6b3103a789772e3299107eed
Andre Barros <andrebarros7201@gmail.com>
2025-10-23 17:27:35 +0100

    Add `SubjectCompleteDto` for detailed subject representation
    - Introduced `SubjectCompleteDto` to encapsulate comprehensive details of a subject, including `Id`, `Name`, `ProfessorId`, `Professor`, `ClassId`, `Class`, and a list of `Grades`.

---
+++ server/DTOs/Subject/SubjectCompleteDto.cs
@@ -0,0 +1,13 @@
+using Server.DTOs.Grade;
+
+namespace Server.DTOs.Subject;
+
+public class SubjectCompleteDto {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int ProfessorId { get; set; }
+    public string Professor { get; set; } = string.Empty;
+    public int ClassId { get; set; }
+    public string Class { get; set; } = string.Empty;
+    public List<GradeSimpleDto> Grades { get; set; } = [];
+}
