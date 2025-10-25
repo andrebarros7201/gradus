80d2eecaa8a9a2aae2bfd6e95f01017376a0d148
Andre Barros <andrebarros7201@gmail.com>
2025-10-25 17:15:37 +0100

    Import `main.scss` in `layout.tsx` to include foundational SCSS styles

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
+import "@/styles/main.scss";
 import { Header } from "../components/header/Header";
 
 const geistSans = Geist({
