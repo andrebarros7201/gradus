ea6fd270483161a9ef711a81c32aa5306c451577
Andre Barros <andrebarros7201@gmail.com>
2025-10-26 18:38:33 +0000

    feat: add link to register and login page and add style

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,7 +1,15 @@
-import classes from './header.module.scss'
+import Link from 'next/link';
+import classes from './header.module.scss';
 
 export const Header = () => {
-    return <div className={classes.header}>
-        <h1>Gradus</h1>
+  return (
+    <div className={classes.header}>
+      <h2>Gradus</h2>
+      <div className={classes['header-group']}>
+        <Link href={'/register'}>Register</Link>
+        <Link href={'/login'}>Login</Link>
+      </div>
     </div>
-}
+  );
+};
+
