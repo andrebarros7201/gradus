1a5a0acaf5d1a62ab3ed9c3a4bf92688347ec4ed
Andre Barros <andrebarros7201@gmail.com>
2025-10-28 20:19:34 +0000

    feat: add user login

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { Page } from '@/components/page/Page';
 import classes from './page.module.scss';
@@ -6,17 +6,35 @@ import { Form } from '@/components/ui/form/Form';
 import { FormEvent, useRef } from 'react';
 import { Button } from '@/components/ui/button/Button';
 import { Input } from '@/components/ui/input/Input';
+import { useDispatch } from 'react-redux';
+import { RootDispatch } from '@/redux/store';
+import { loginUser } from '@/redux/slices/userSlice';
+import { showNotification } from '@/redux/slices/notificationSlice';
 
 export default function LoginPage() {
+  const dispatch = useDispatch<RootDispatch>();
+
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  function onSubmit(e: FormEvent) {
+  async function onSubmit(e: FormEvent) {
+    // Prevent Default Behavior
     e.preventDefault();
 
     const username = usernameRef.current?.value;
     const password = passwordRef.current?.value;
+
+    // Cancel if values are missing
+    if (!username || !password) return;
+
+    try {
+      await dispatch(loginUser({ username, password })).unwrap();
+      dispatch(showNotification({ message: 'Login successful!', type: 'success' }));
+    } catch (err) {
+      dispatch(showNotification({ message: (err as string) || 'Login failed!', type: 'error' }));
+    }
   }
+
   return (
     <Page>
       <Form label="Login" onSubmit={(e) => onSubmit(e)}>
