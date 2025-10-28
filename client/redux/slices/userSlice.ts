9a68bed830b03b5339dcef6c0e18e8cdf8ee0904
Andre Barros <andrebarros7201@gmail.com>
2025-10-28 20:19:59 +0000

    feat: add user login async thunk

--- client/redux/slices/userSlice.ts
+++ client/redux/slices/userSlice.ts
@@ -1,17 +1,54 @@
-import { IUserSlice } from "@/types/IUserSlice";
-import { createSlice } from "@reduxjs/toolkit";
+import { IUser } from '@/types/IUser';
+import { IUserSlice } from '@/types/IUserSlice';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios, { AxiosError } from 'axios';
 
 // Initial State Values
 const initialState: IUserSlice = {
   isLoading: false,
+  isAuthenticated: false,
   user: null,
 };
 
+// User Login
+export const loginUser = createAsyncThunk<
+  { user: IUser },
+  { username: string; password: string },
+  { rejectValue: string }
+>('user/loginUser', async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(`${process.env.SERVER_URL}/api/auth/login`, {
+      username,
+      password,
+    });
+    const user = response.data.data;
+    return { user };
+  } catch (e) {
+    const error = e as AxiosError;
+    return rejectWithValue((error.response?.data as string) || 'Login failed');
+  }
+});
+
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {},
-  extraReducers: (builder) => builder,
+  extraReducers: (builder) =>
+    builder
+      // User Login
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.user;
+        state.isAuthenticated = true;
+      })
+      .addCase(loginUser.rejected, (state) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      }),
 });
 
 export const userReducer = userSlice.reducer;
