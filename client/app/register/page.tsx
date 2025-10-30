3f8105d7583043620ac5773cde2e51716a20ddae
Andre Barros <andrebarros7201@gmail.com>
2025-10-30 16:05:46 +0000

    refactor(user/register): add zod validation

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -14,6 +14,7 @@ import { setNotification } from '@/redux/slices/notificationSlice';
 import { userRegister } from '@/redux/slices/userSlice';
 import { UserRegisterSelect } from '@/components/ui/userRegisterSelect/UserRegisterSelect';
 import { INotification } from '@/types/INotificationSlice';
+import * as z from 'zod';
 
 export default function RegisterPage() {
   const { user } = useSelector((state: RootState) => state.user);
@@ -27,6 +28,32 @@ export default function RegisterPage() {
   const roleRef = useRef<HTMLSelectElement>(null);
   const schoolYearRef = useRef<HTMLSelectElement>(null);
 
+  const userRegisterSchema = z
+    .object({
+      name: z.string().nonempty('Name is required').min(3, 'Name must have at least 3 characters'),
+      username: z
+        .string()
+        .nonempty('Username is required')
+        .min(3, 'Username must have at least 3 characters'),
+      password: z
+        .string()
+        .nonempty('Password is required')
+        .min(3, 'Password must have at least 3 characters'),
+      role: z.string(),
+      schoolYear: z.string().optional(), // Only required if role is Class
+    })
+    // Requires School Year if role is class
+    .refine(
+      (data) => {
+        if (data.role === '1') return !!data.schoolYear;
+        return true;
+      },
+      {
+        message: 'School Year is required if user role is Class',
+        path: ['schoolYear'],
+      },
+    );
+
   async function onSubmit(e: FormEvent) {
     // Prevent default form submission behavior
     e.preventDefault();
@@ -37,18 +64,24 @@ export default function RegisterPage() {
     const role = roleRef.current?.value;
     const schoolYear = schoolYearRef.current?.value;
 
-    console.log({ name, username, password, role, schoolYear });
-
-    if (!name || !username || !password || !role || (Number(role) === 1 && !schoolYear)) return;
-
     try {
+      // Validate data
+      const result = userRegisterSchema.safeParse({ username, name, password, role, schoolYear });
+
+      // Check for errors
+      if (!result.success) {
+        const firstError = result.error.issues[0].message || 'Invalid Input';
+        dispatch(setNotification({ type: 'error', message: firstError }));
+        return;
+      }
+
       const response = await dispatch(
         userRegister({
-          name,
-          username,
-          password,
-          role: Number(role),
-          schoolYear: schoolYear || '',
+          name: result.data.name,
+          username: result.data.username,
+          password: result.data.password,
+          role: Number(result.data.role),
+          schoolYear: result.data.schoolYear,
         }),
       ).unwrap();
 
