b0da838edcc6e7b1e3975bddebf9e1623aee54eb
Andre Barros <andrebarros7201@gmail.com>
2025-10-30 22:32:07 +0000

    feat(classes/fetchAllClasses): add fetchAllClasses async thunk

--- client/redux/slices/classSlice.ts
+++ client/redux/slices/classSlice.ts
@@ -1,16 +1,47 @@
+import { IClassSimple } from '@/types/IClass';
 import { IClassSlice } from '@/types/IClassSlice';
-import { createSlice } from '@reduxjs/toolkit';
+import { INotification } from '@/types/INotificationSlice';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState: IClassSlice = {
   isLoading: false,
   classes: [],
 };
 
+export const fetchAllClasses = createAsyncThunk<
+  { classes: IClassSimple[] },
+  void,
+  { rejectValue: { notification: INotification } }
+>('class/fetchAllClasses', async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${process.env.SERVER_URL}/api/class`, {
+      withCredentials: true,
+    });
+    const { data } = response.data;
+    return { classes: data };
+  } catch (e) {
+    return rejectWithValue({ notification: { type: 'error', message: 'Failed to load classes' } });
+  }
+});
+
 const classSlice = createSlice({
   name: 'classSlice',
   initialState,
   reducers: {},
-  extraReducers: (build) => build,
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAllClasses.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchAllClasses.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.classes = action.payload.classes;
+      })
+      .addCase(fetchAllClasses.rejected, (state) => {
+        state.isLoading= false
+        state.classes = []
+      }),
 });
 
 export const classReducer = classSlice.reducer;
