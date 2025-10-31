4471827c1886f96deb896763eed9105237c663fd
Andre Barros <andrebarros7201@gmail.com>
2025-10-31 22:04:24 +0000

    feat(classItem): add ClassItem component

---
+++ client/components/classList/classListItem/ClassListItem.tsx
@@ -0,0 +1,22 @@
+import classes from './classListItem.module.scss';
+import { Button } from '@/components/ui/button/Button';
+import { IClassSimple } from '@/types/IClass';
+
+type Props = {
+  item: IClassSimple;
+};
+
+export const ClassListItem = ({ item }: Props) => {
+  return (
+    <div className={classes['item']}>
+      <p>{item.id}</p>
+      <p>{item.name}</p>
+      <p>{item.schoolYear}</p>
+      <p>{item.isActive === true ? 'Yes' : 'No'}</p>
+      <div className={classes['item__buttons']}>
+        <Button label={'Update'} variant="secondary" />
+        <Button label={'Delete'} variant="danger" />
+      </div>
+    </div>
+  );
+};
