e5266d6c56248440ee208d52d88cd739d54ca416
Andre Barros <andrebarros7201@gmail.com>
2025-10-31 20:34:46 +0000

    feat(adminDashboard): add cards to admin dashboard

--- client/components/admin/dashboard/AdminDashboard.tsx
+++ client/components/admin/dashboard/AdminDashboard.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import { Card } from '@/components/ui/card/Card';
 import classes from './adminDashboard.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
 
 export const AdminDashboard = () => {
-  return <main className={classes.wrapper}></main>;
+  const { user } = useSelector((state: RootState) => state.user);
+  return (
+    <main className={classes.dashboard}>
+      <h3>Welcome back, {user?.name}</h3>
+      <div className={classes['dashboard__cards']}>
+        <Card label={'Classes'} description={'Manage classes'} href={'/class'} />
+        <Card label={'Classes'} description={'Manage classes'} href={'/class'} />
+        <Card label={'Classes'} description={'Manage classes'} href={'/class'} />
+      </div>
+    </main>
+  );
 };
