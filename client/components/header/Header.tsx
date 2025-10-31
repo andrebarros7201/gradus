6a626f8907246c407c2ec51256857c6469a03e24
Andre Barros <andrebarros7201@gmail.com>
2025-10-31 18:41:21 +0000

    feat: add new HeaderAuthItems component to header and change items display

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -3,8 +3,8 @@ import { useSelector } from 'react-redux';
 import classes from './header.module.scss';
 import { RootState } from '@/redux/store';
 import { HeaderItem } from './headerItem/HeaderItem';
-import { HeaderLogoutButton } from './headerLogoutButton/HeaderLogoutButton';
 import { Role } from '@/types/RoleEnum';
+import { HeaderAuthItems } from './headerItem/authHeaderItems/HeaderAuthItems';
 
 export const Header = () => {
   const { isAuthenticated, user, isLoading } = useSelector((state: RootState) => state.user);
@@ -12,19 +12,20 @@ export const Header = () => {
   return (
     <div className={classes.header}>
       <h2>Gradus</h2>
-      {isLoading ? null : isAuthenticated ? (
-        <div className={classes['header-group']}>
-          {
-            // Only Admins can create new users
-            user && user.role === Role.Admin && <HeaderItem label="Create User" href="/register" />
-          }
-          <HeaderLogoutButton />
-        </div>
-      ) : (
-        <div className={classes['header-group']}>
-          <HeaderItem label="Login" href="/login" />
-        </div>
-      )}
+      {isLoading
+        ? null // Don't render if still loading
+        : isAuthenticated && (
+            <div className={classes['header-group']}>
+              <HeaderItem label={'Dashboard'} href={'/dashboard'} />
+              {
+                // Only Admins can create new users
+                user && user.role === Role.Admin && (
+                  <HeaderItem label="Create User" href="/register" />
+                )
+              }
+            </div>
+          )}
+      <HeaderAuthItems />
     </div>
   );
 };
