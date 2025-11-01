4b9a7530a19ce6a3ab9ae4f4950e419d43112ec8
Andre Barros <andrebarros7201@gmail.com>
2025-11-01 15:26:36 +0000

    feat(client/class/delete): add class deleting functionality

--- client/components/classList/classListItem/ClassListItem.tsx
+++ client/components/classList/classListItem/ClassListItem.tsx
@@ -1,12 +1,30 @@
+import { deleteClass } from '@/redux/slices/classSlice';
 import classes from './classListItem.module.scss';
 import { Button } from '@/components/ui/button/Button';
+import { RootDispatch } from '@/redux/store';
 import { IClassSimple } from '@/types/IClassSimple';
+import { useDispatch } from 'react-redux';
+import { setNotification } from '@/redux/slices/notificationSlice';
+import { INotification } from '@/types/INotificationSlice';
 
 type Props = {
   item: IClassSimple;
 };
 
 export const ClassListItem = ({ item }: Props) => {
+  const dispatch = useDispatch<RootDispatch>();
+
+  async function handleDelete() {
+    try {
+      const response = await dispatch(deleteClass({ userId: item.userId })).unwrap();
+      const { notification } = response;
+      dispatch(setNotification(notification));
+    } catch (e) {
+      const error = e as { notification: INotification };
+      dispatch(setNotification(error.notification));
+    }
+  }
+
   return (
     <div className={classes['item']}>
       <p>{item.id}</p>
@@ -15,7 +33,7 @@ export const ClassListItem = ({ item }: Props) => {
       <p>{item.isActive === true ? 'Yes' : 'No'}</p>
       <div className={classes['item__buttons']}>
         <Button label={'Update'} variant="secondary" />
-        <Button label={'Delete'} variant="danger" />
+        <Button label={'Delete'} variant="danger" onClick={handleDelete} />
       </div>
     </div>
   );
--- client/redux/slices/classSlice.ts
+++ client/redux/slices/classSlice.ts
@@ -2,13 +2,15 @@ import { IClassSimple } from '@/types/IClassSimple';
 import { IClassSlice } from '@/types/IClassSlice';
 import { INotification } from '@/types/INotificationSlice';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import { callbackify } from 'util';
 
 const initialState: IClassSlice = {
   isLoading: false,
   classes: [],
 };
 
+// Fetch All Classes
 export const fetchAllClasses = createAsyncThunk<
   { classes: IClassSimple[] },
   void,
@@ -26,12 +28,36 @@ export const fetchAllClasses = createAsyncThunk<
   }
 });
 
+// Delete Class
+export const deleteClass = createAsyncThunk<
+  { notification: INotification; userId: number },
+  { userId: number },
+  { rejectValue: { notification: INotification } }
+>('class/deleteClass', async ({ userId }, { rejectWithValue }) => {
+  try {
+    const response = await axios.delete(`${process.env.SERVER_URL}/api/user/${userId}`, {
+      withCredentials: true,
+    });
+    const { data } = response.data;
+    return { notification: { type: 'success', message: data }, userId };
+  } catch (e) {
+    const error = e as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      notification: {
+        type: 'error',
+        message: error.response?.data.message ?? 'Failed to delete class',
+      },
+    });
+  }
+});
+
 const classSlice = createSlice({
   name: 'classSlice',
   initialState,
   reducers: {},
   extraReducers: (builder) =>
     builder
+      // Fetch All Classes
       .addCase(fetchAllClasses.pending, (state) => {
         state.isLoading = true;
       })
@@ -42,6 +68,20 @@ const classSlice = createSlice({
       .addCase(fetchAllClasses.rejected, (state) => {
         state.isLoading = false;
         state.classes = [];
+      })
+      // Delete Class
+      .addCase(deleteClass.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteClass.fulfilled, (state, action) => {
+        const { userId } = action.payload;
+        state.isLoading = true;
+        const classIndex = state.classes.findIndex((c) => c.userId === userId);
+        if (classIndex === -1) return;
+        state.classes = state.classes.splice(classIndex, 1);
+      })
+      .addCase(deleteClass.rejected, (state) => {
+        state.isLoading = false;
       }),
 });
 
