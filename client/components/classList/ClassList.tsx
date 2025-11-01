2adba9d7f6896eb8ba9f9c175a00da3e3803f300
Andre Barros <andrebarros7201@gmail.com>
2025-11-01 20:26:47 +0000

    refactor(classList): replace classList with a single reusable List component and create ClassListItem

--- client/components/classList/ClassList.tsx
+++ client/components/classList/ClassList.tsx
@@ -2,27 +2,15 @@
 
 import classes from './classList.module.scss';
 import { RootState } from '@/redux/store';
-import { IClassSimple } from '@/types/IClassSimple';
 import { useSelector } from 'react-redux';
-import { ClassListItem } from './classListItem/ClassListItem';
-
-// TODO create a single reusable list component that accepts user/grade/class/subject list
+import { List } from '../ui/list/List';
 
 export const ClassList = () => {
   const { classes: classList } = useSelector((state: RootState) => state.class);
 
   return (
     <main className={classes['list']}>
-      <div className={classes['list__header']}>
-        <p>ID</p>
-        <p>Name</p>
-        <p>School Year</p>
-        <p>Is Active</p>
-        <p>Actions</p>
-      </div>
-      {classList.map((item: IClassSimple) => (
-        <ClassListItem key={item.id} item={item} />
-      ))}
+    <List type={'class'} list={classList} />
     </main>
   );
 };
---
+++ client/components/ui/list/List.tsx
@@ -0,0 +1,41 @@
+import classes from './list.module.scss';
+import { IClassSimple } from '@/types/IClassSimple';
+import { ISubjectSimple } from '@/types/ISubjectSimple';
+import { ClassListItem } from './class/ClassListItem';
+import { SubjectListItem } from './subject/SubjectListItem';
+
+type Class = {
+  type: 'class';
+  list: IClassSimple[];
+};
+
+type Subject = {
+  type: 'subject';
+  list: ISubjectSimple[];
+};
+
+type Props = Class | Subject;
+
+export const List = ({ type, list }: Props) => {
+  // Get headers according to list type
+  const headers =
+    type === 'class'
+      ? ['Id', 'Name', 'School Year', 'Is Active', 'Actions']
+      : type === 'subject'
+      ? ['Id', 'Name', 'Professor', 'Actions']
+      : [];
+
+  return (
+    <div className={classes.list}>
+      <div className={classes['list__header']}>
+        {headers.map((header) => (
+          <p key={header} className={classes['list__header-item']}>
+            {header}
+          </p>
+        ))}
+      </div>
+      {type === 'class' && list.map((item) => <ClassListItem key={item.id} item={item} />)}
+      {type === 'subject' && list.map((item) => <SubjectListItem key={item.id} item={item} />)}
+    </div>
+  );
+};
