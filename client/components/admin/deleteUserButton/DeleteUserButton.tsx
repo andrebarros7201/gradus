0ae29b9ddd95f45107ef7e3a1c5adfa41a20b376
Andre Barros <andrebarros7201@gmail.com>
2025-11-02 14:18:49 +0000

    refactor(user/delete): create a single reusable delete user button

---
+++ client/components/admin/deleteUserButton/DeleteUserButton.tsx
@@ -0,0 +1,29 @@
+import { Button } from '@/components/ui/button/Button';
+import { setNotification } from '@/redux/slices/notificationSlice';
+import { deleteUser } from '@/redux/slices/userSlice';
+import { RootDispatch } from '@/redux/store';
+import { IClassSimple } from '@/types/IClassSimple';
+import { INotification } from '@/types/INotificationSlice';
+import { useDispatch } from 'react-redux';
+
+type Props = {
+  item: IClassSimple;
+  type: 'class' | 'admin' | 'professor';
+};
+
+export const DeleteUserButton = ({ item, type }: Props) => {
+  const dispatch = useDispatch<RootDispatch>();
+
+  async function handleDelete() {
+    try {
+      const response = await dispatch(deleteUser({ userId: item.userId, type })).unwrap();
+      const { notification } = response;
+      dispatch(setNotification(notification));
+    } catch (e) {
+      const error = e as { notification: INotification };
+      dispatch(setNotification(error.notification));
+    }
+  }
+
+  return <Button label={'Delete'} variant="danger" onClick={handleDelete} />;
+};
