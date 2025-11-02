e9d9953db3ce0b2263c7372a566c4244ccf07f50
Andre Barros <andrebarros7201@gmail.com>
2025-11-02 19:29:42 +0000

    feat(select): add optional isDisabled property

--- client/components/ui/select/Select.tsx
+++ client/components/ui/select/Select.tsx
@@ -6,13 +6,19 @@ type Props = {
   children: ReactNode | ReactNode[];
   ref: React.Ref<HTMLSelectElement>;
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  isDisabled?: boolean;
 };
 
-export const Select = ({ label, children, ref, onChange }: Props) => {
+export const Select = ({ label, children, ref, onChange, isDisabled = false }: Props) => {
   return (
     <div className={classes.wrapper}>
       <label className={classes['wrapper__label']}>{label}</label>
-      <select className={classes['wrapper__select']} ref={ref} onChange={onChange}>
+      <select
+        className={classes['wrapper__select']}
+        ref={ref}
+        onChange={onChange}
+        disabled={isDisabled}
+      >
         {children}
       </select>
     </div>
