ef42d87107731b178a22765c7c1724dc71b5e0a5
Andre Barros <andrebarros7201@gmail.com>
2025-11-03 12:32:55 +0000

    feat(modal): add Modal component

---
+++ client/components/ui/modal/Modal.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import classes from './modal.module.scss';
+
+type Props = {
+  children: ReactNode | ReactNode[];
+  onClose: () => void;
+  title: string;
+};
+
+export const Modal = ({ children, onClose, title }: Props) => {
+  return (
+    <div className={classes.modal} onClick={onClose}>
+      <div className={classes['modal__content']} onClick={(e) => e.stopPropagation()}>
+        <div className={classes['modal__top']}>
+          <h4 className={classes['modal__title']}>{title}</h4>
+          <button className={classes['modal__close-button']} onClick={onClose}>
+            X
+          </button>
+        </div>
+        {children}
+      </div>
+    </div>
+  );
+};
