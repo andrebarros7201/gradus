0f8d8dabddb060c44ce7c7564b46c67564e86b73
Andre Barros <andrebarros7201@gmail.com>
2025-11-03 16:59:44 +0000

    feat(UserPatchDto): add optional SchoolYear property

--- server/DTOs/User/UserPatchDto.cs
+++ server/DTOs/User/UserPatchDto.cs
@@ -4,4 +4,5 @@ public class UserPatchDto {
     public string Name { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
     public string? Password { get; set; } = string.Empty;
+    public string? SchoolYear {get; set; } = string.Empty;
 }
