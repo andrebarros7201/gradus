ba18431a3c81a811569cefd866b562eb04fc4b76
Andre Barros <andrebarros7201@gmail.com>
2025-11-05 19:50:01 +0000

    feat(class/classId): dispatch clearCurrentClass on dismount

--- client/app/dashboard/class/[classId]/page.tsx
+++ client/app/dashboard/class/[classId]/page.tsx
@@ -4,7 +4,7 @@ import classes from './page.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootDispatch, RootState } from '@/redux/store';
 import { use, useEffect } from 'react';
-import { fetchCurrentClass } from '@/redux/slices/classSlice';
+import { clearCurrentClass, fetchCurrentClass } from '@/redux/slices/classSlice';
 
 type Props = {
   params: Promise<{ classId: string }>;
@@ -18,6 +18,11 @@ export default function ClassPage({ params }: Props) {
   // Fetch Current Class
   useEffect(() => {
     dispatch(fetchCurrentClass({ classId: Number(classId) }));
+
+    // Clear current class value on dismount
+    return () => {
+      dispatch(clearCurrentClass());
+    };
   }, [dispatch, classId]);
 
   return <main>{currentClass?.name}</main>;
