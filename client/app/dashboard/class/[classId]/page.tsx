e1980a00fd07cbd2b5d1ed85da95c821124507b0
Andre Barros <andrebarros7201@gmail.com>
2025-11-05 19:39:14 +0000

    feat(/class/classId): create page with boilerplate and fetch class

---
+++ client/app/dashboard/class/[classId]/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import classes from './page.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootDispatch, RootState } from '@/redux/store';
+import { use, useEffect } from 'react';
+import { fetchCurrentClass } from '@/redux/slices/classSlice';
+
+type Props = {
+  params: Promise<{ classId: string }>;
+};
+
+export default function ClassPage({ params }: Props) {
+  const { currentClass } = useSelector((state: RootState) => state.class);
+  const { classId } = use(params);
+  const dispatch = useDispatch<RootDispatch>();
+
+  // Fetch Current Class
+  useEffect(() => {
+    dispatch(fetchCurrentClass({ classId: Number(classId) }));
+  }, [dispatch, classId]);
+
+  return <main>{currentClass?.name}</main>;
+}
