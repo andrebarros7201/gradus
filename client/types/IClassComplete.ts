0eca1864d3e71f1035fd031015df1afe873f6d19
Andre Barros <andrebarros7201@gmail.com>
2025-11-05 16:42:43 +0000

    feat(IClassComplete): create IClassComplete interface

---
+++ client/types/IClassComplete.ts
@@ -0,0 +1,12 @@
+import { IStudentSimple } from "./IStudentSimple";
+import { ISubjectSimple } from "./ISubjectSimple";
+
+export interface IClassComplete {
+    id: number,
+    userId: number,
+    name: string,
+    schoolYear: string,
+    isActive: boolean,
+    students: IStudentSimple[]
+    subjects: ISubjectSimple[]
+}
