d98909cbee4d8d5599d52ec651bf71b090247af0
Andre Barros <andrebarros7201@gmail.com>
2025-11-06 20:20:38 +0000

    feat(classDetails): create ClassDetails component

---
+++ client/components/class/classDetails/ClassDetails.tsx
@@ -0,0 +1,26 @@
+import classes from './classDetails.module.scss';
+
+import { IClassComplete } from '@/types/IClassComplete';
+
+type Props = {
+  item: IClassComplete;
+};
+
+export const ClassDetails = ({ item }: Props) => {
+  return (
+    <main className={classes.classDetails}>
+      <div className={classes['classDetails__field']}>
+        <p className={classes['classDetails__label']}>Name</p>
+        <p className={classes['classDetails__value']}>{item.name}</p>
+      </div>
+      <div className={classes['classDetails__field']}>
+        <p className={classes['classDetails__label']}>School Year</p>
+        <p className={classes['classDetails__value']}>{item.schoolYear}</p>
+      </div>
+      <div className={classes['classDetails__field']}>
+        <p className={classes['classDetails__label']}>Is Active</p>
+        <p className={classes['classDetails__value']}>{item.isActive === true ? 'Yes' : 'No'}</p>
+      </div>
+    </main>
+  );
+};
