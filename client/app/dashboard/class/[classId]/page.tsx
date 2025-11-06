7bd78f99c8082d24873a787252640e5749c6e3f6
Andre Barros <andrebarros7201@gmail.com>
2025-11-06 20:21:09 +0000

    feat(class/classId): use ClassDetails component

--- client/app/dashboard/class/[classId]/page.tsx
+++ client/app/dashboard/class/[classId]/page.tsx
@@ -5,13 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootDispatch, RootState } from '@/redux/store';
 import { use, useEffect } from 'react';
 import { clearCurrentClass, fetchCurrentClass } from '@/redux/slices/classSlice';
+import { Page } from '@/components/page/Page';
+import { ClassDetails } from '@/components/class/classDetails/ClassDetails';
 
 type Props = {
   params: Promise<{ classId: string }>;
 };
 
 export default function ClassPage({ params }: Props) {
-  const { currentClass } = useSelector((state: RootState) => state.class);
+  const { currentClass, isLoading } = useSelector((state: RootState) => state.class);
   const { classId } = use(params);
   const dispatch = useDispatch<RootDispatch>();
 
@@ -25,5 +27,12 @@ export default function ClassPage({ params }: Props) {
     };
   }, [dispatch, classId]);
 
-  return <main>{currentClass?.name}</main>;
+  return (
+    !isLoading &&
+    currentClass && (
+      <Page>
+        <ClassDetails item={currentClass} />
+      </Page>
+    )
+  );
 }
