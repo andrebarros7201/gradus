19e8a83393a7a76178933cf0412bb04874f4d184
Andre Barros <andrebarros7201@gmail.com>
2025-11-07 20:20:11 +0000

    feat(IClassComplete): add username property

--- client/types/IClassComplete.ts
+++ client/types/IClassComplete.ts
@@ -1,12 +1,13 @@
-import { IStudentSimple } from "./IStudentSimple";
-import { ISubjectSimple } from "./ISubjectSimple";
+import { IStudentSimple } from './IStudentSimple';
+import { ISubjectSimple } from './ISubjectSimple';
 
 export interface IClassComplete {
-    id: number,
-    userId: number,
-    name: string,
-    schoolYear: string,
-    isActive: boolean,
-    students: IStudentSimple[]
-    subjects: ISubjectSimple[]
+  id: number;
+  userId: number;
+  name: string;
+  username: string;
+  schoolYear: string;
+  isActive: boolean;
+  students: IStudentSimple[];
+  subjects: ISubjectSimple[];
 }
