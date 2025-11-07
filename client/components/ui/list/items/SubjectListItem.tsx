07cb64d8cf779613f4bfa9c807cd68b11fc4d5c1
Andre Barros <andrebarros7201@gmail.com>
2025-11-07 11:24:11 +0000

    feat(SubjectItemList): add style to each field and add ButtonLink to /subject/itemId

--- client/components/ui/list/items/SubjectListItem.tsx
+++ client/components/ui/list/items/SubjectListItem.tsx
@@ -1,6 +1,7 @@
 import classes from './listItem.module.scss';
 import { Button } from '../../button/Button';
 import { ISubjectSimple } from '@/types/ISubjectSimple';
+import { ButtonLink } from '../../buttonLink/ButtonLink';
 
 type Props = {
   item: ISubjectSimple;
@@ -9,15 +10,15 @@ type Props = {
 export const SubjectListItem = ({ item }: Props) => {
   return (
     <div className={classes['item']}>
-      <div>
+      <div className={classes['item__field']}>
         <p className={classes['item__label']}>Id</p>
         <p>{item.id}</p>
       </div>
-      <div>
+      <div className={classes['item__field']}>
         <p className={classes['item__label']}>Name</p>
         <p>{item.name}</p>
       </div>
-      <div>
+      <div className={classes['item__field']}>
         <p className={classes['item__label']}>Professor</p>
         <p>{item.professor}</p>
       </div>
@@ -25,6 +26,7 @@ export const SubjectListItem = ({ item }: Props) => {
         <p className={classes['item__label']}>Actions</p>
         <Button label={'Update'} variant="secondary" />
         <Button label={'Delete'} variant="danger" />
+        <ButtonLink label={'Go to'} href={`/subject/${item.id}`} />
       </div>
     </div>
   );
