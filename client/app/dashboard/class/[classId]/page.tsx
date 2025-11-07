5b2cc8db5669bc1b365f312d778bc68c263e177a
Andre Barros <andrebarros7201@gmail.com>
2025-11-07 19:25:14 +0000

    refactor(/class/classId): get currentClass from currentClassSlice

--- client/app/dashboard/class/[classId]/page.tsx
+++ client/app/dashboard/class/[classId]/page.tsx
@@ -4,17 +4,17 @@ import classes from './page.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootDispatch, RootState } from '@/redux/store';
 import { use, useEffect } from 'react';
-import { clearCurrentClass, fetchCurrentClass } from '@/redux/slices/classSlice';
 import { Page } from '@/components/page/Page';
 import { ClassDetails } from '@/components/class/classDetails/ClassDetails';
 import { ClassViewToggler } from '@/components/class/classViewToggler/ClassViewToggler';
+import { clearCurrentClass, fetchCurrentClass } from '@/redux/slices/currentClassSlice';
 
 type Props = {
   params: Promise<{ classId: string }>;
 };
 
 export default function ClassPage({ params }: Props) {
-  const { currentClass, isLoading } = useSelector((state: RootState) => state.class);
+  const { currentClass, isLoading } = useSelector((state: RootState) => state.currentClass);
   const { classId } = use(params);
   const dispatch = useDispatch<RootDispatch>();
 
@@ -28,13 +28,12 @@ export default function ClassPage({ params }: Props) {
     };
   }, [dispatch, classId]);
 
-  return (
-    !isLoading &&
-    currentClass && (
+  if (!isLoading && currentClass) {
+    return (
       <Page>
         <ClassDetails item={currentClass} />
         <ClassViewToggler item={currentClass} />
       </Page>
-    )
-  );
+    );
+  }
 }
