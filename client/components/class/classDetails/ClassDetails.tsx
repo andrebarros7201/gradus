705492ee777f90f125185be7cc60d3a60f389ea8
Andre Barros <andrebarros7201@gmail.com>
2025-11-11 19:31:17 +0000

    feat(ClassDetails): add Delete button

--- client/components/class/classDetails/ClassDetails.tsx
+++ client/components/class/classDetails/ClassDetails.tsx
@@ -1,19 +1,25 @@
-import { ButtonLink } from '@/components/ui/buttonLink/ButtonLink';
+'use client';
 import classes from './classDetails.module.scss';
-
+import { ButtonLink } from '@/components/ui/buttonLink/ButtonLink';
 import { IClassComplete } from '@/types/IClassComplete';
 import { UpdateUserButton } from '@/components/admin/userButton/updateUserButton/UpdateUserButton';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import { Role } from '@/types/RoleEnum';
+import { DeleteUserButton } from '@/components/admin/userButton/deleteUserButton/DeleteUserButton';
 
 type Props = {
   item: IClassComplete;
 };
 
 export const ClassDetails = ({ item }: Props) => {
+  const { user } = useSelector((state: RootState) => state.user);
   return (
     <main className={classes.classDetails}>
       <div className={classes['classDetails__button']}>
         <ButtonLink label={'Go Back'} href={'/dashboard/class'} />
-        <UpdateUserButton type={'class'} item={item} />
+        {user?.role === Role.Admin && <UpdateUserButton type={'class'} item={item} />}
+        {user?.role === Role.Admin && <DeleteUserButton type={'class'} item={item} />}
       </div>
       <div className={classes['classDetails__field']}>
         <p className={classes['classDetails__label']}>Name</p>
