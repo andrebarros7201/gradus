cc8b751931fbd5a97006d999a64564624c867e81
Andre Barros <andrebarros7201@gmail.com>
2025-11-12 14:56:40 +0000

    fix(IEvaluationService): remove get evaluation by class id

--- server/Interfaces/Services/IEvaluationService.cs
+++ server/Interfaces/Services/IEvaluationService.cs
@@ -1,7 +1,6 @@
 using Server.Results;
 
 public interface IEvaluationService {
-    public Task<ServiceResult<List<EvaluationDto>>> GetEvaluationsByClassId(int currentUserId, int classId);
     public Task<ServiceResult<EvaluationDto>> GetEvaluationById(int currentUserId, int evaluationId);
     public Task<ServiceResult<EvaluationDto>> CreateEvaluation(int currentUserId, EvaluationCreateDto dto);
     public Task<ServiceResult<EvaluationDto>> UpdateEvaluation(int currentUserId, int evaluationId, EvaluationCreateDto dto);
