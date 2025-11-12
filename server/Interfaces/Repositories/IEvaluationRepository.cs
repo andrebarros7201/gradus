92766040fe80570198192e513701ab3156d692b1
Andre Barros <andrebarros7201@gmail.com>
2025-11-12 15:52:16 +0000

    fix(Evaluation) change delete return from EvaluationDto to bool

--- server/Interfaces/Repositories/IEvaluationRepository.cs
+++ server/Interfaces/Repositories/IEvaluationRepository.cs
@@ -2,5 +2,5 @@ public interface IEvaluationRepository {
     public Task<Evaluation?> GetEvaluationById(int evaluationId);
     public Task<Evaluation> CreateEvaluation(Evaluation evaluation);
     public Task<Evaluation> UpdateEvaluation(Evaluation evaluation);
-    public Task<bool> DeleteEvaluation(int evaluationId);
+    public Task<bool> DeleteEvaluation(Evaluation evaluation);
 }
--- server/Interfaces/Services/IEvaluationService.cs
+++ server/Interfaces/Services/IEvaluationService.cs
@@ -4,5 +4,5 @@ public interface IEvaluationService {
     public Task<ServiceResult<EvaluationDto>> GetEvaluationById(int currentUserId, int evaluationId);
     public Task<ServiceResult<EvaluationDto>> CreateEvaluation(int currentUserId, EvaluationCreateDto dto);
     public Task<ServiceResult<EvaluationDto>> UpdateEvaluation(int currentUserId, int evaluationId, EvaluationCreateDto dto);
-    public Task<ServiceResult<EvaluationDto>> DeleteEvaluationById(int currentUserId, int evaluationId);
+    public Task<ServiceResult<bool>> DeleteEvaluationById(int currentUserId, int evaluationId);
 }
