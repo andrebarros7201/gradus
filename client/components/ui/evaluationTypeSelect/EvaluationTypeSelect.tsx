ded79742bdec003a8a78becb317a8cb4f28d6c25
Andre Barros <andrebarros7201@gmail.com>
2025-11-17 12:31:07 +0000

    feat(EvaluationTypeSelect): add select with evaluation type options

---
+++ client/components/ui/evaluationTypeSelect/EvaluationTypeSelect.tsx
@@ -0,0 +1,24 @@
+import { Ref } from 'react';
+import { Select } from '../select/Select';
+import { EvaluationType } from '@/types/EvaluationEnum';
+
+type Props = {
+  ref: Ref<HTMLSelectElement>;
+};
+
+export const EvaluationTypeSelect = ({ ref }: Props) => {
+  const evaluation_types = Object.entries(EvaluationType)
+  .filter(([key]) => isNaN(Number(key)))
+  .map(([key, value]) => ({
+    label: key,
+    value: value,
+  }));
+
+  return (
+    <Select label={'Evaluation Type'} ref={ref}>
+      {evaluation_types.map((t) => (
+        <option value={t.value} key={t.value}>{t.label}</option>
+      ))}
+    </Select>
+  );
+};
