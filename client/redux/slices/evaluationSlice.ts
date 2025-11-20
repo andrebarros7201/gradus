a414cbd69610576d1d7c73b8ebabf9f3d3144ff0
Andre Barros <andrebarros7201@gmail.com>
2025-11-20 20:24:39 +0000

    feat(evaluationSlice): implement create, update, and delete evaluation async thunks

--- client/redux/slices/evaluationSlice.ts
+++ client/redux/slices/evaluationSlice.ts
@@ -1,16 +1,129 @@
+import { IEvaluation } from '@/types/interfaces/IEvaluation';
 import { IEvaluationSlice } from '@/types/slices/IEvaluationSlice';
-import { createSlice } from '@reduxjs/toolkit';
+import { INotification } from '@/types/slices/INotificationSlice';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios, { AxiosError } from 'axios';
 
 const initialState: IEvaluationSlice = {
   evaluations: [],
 };
 
+// Add Evaluation
+export const createEvaluation = createAsyncThunk<
+  { notification: INotification; evaluation: IEvaluation },
+  { subjectId: number; name: string; type: number; date: string },
+  { rejectValue: { notification: INotification } }
+>('evaluation/createEvaluation', async ({ subjectId, date, name, type }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(
+      `${process.env.SERVER_URL}/api/evaluation`,
+      { name, date, evaluationType: type, subjectId },
+      {
+        withCredentials: true,
+      },
+    );
+    return {
+      evaluation: response.data.data,
+      notification: { type: 'success', message: 'Evaluation created successfully' },
+    };
+  } catch (e) {
+    const error = e as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      notification: {
+        type: 'error',
+        message: error.response?.data.message || 'Failed to create evaluation',
+      },
+    });
+  }
+});
+
+// Update Evaluation
+export const updateEvaluation = createAsyncThunk<
+  { notification: INotification; evaluation: IEvaluation },
+  { name: string; type: number; date: string; evaluationId: number; subjectId: number },
+  { rejectValue: { notification: INotification } }
+>(
+  'evaluation/updateEvaluation',
+  async ({ date, name, type, evaluationId, subjectId }, { rejectWithValue }) => {
+    try {
+      const response = await axios.patch(
+        `${process.env.SERVER_URL}/api/evaluation/${evaluationId}`,
+        { name, date, evaluationType: type, subjectId },
+        {
+          withCredentials: true,
+        },
+      );
+      return {
+        evaluation: response.data.data,
+        notification: { type: 'success', message: 'Evaluation updated successfully' },
+      };
+    } catch (e) {
+      const error = e as AxiosError<{ message: string }>;
+      return rejectWithValue({
+        notification: {
+          type: 'error',
+          message: error.response?.data.message || 'Failed to update evaluation',
+        },
+      });
+    }
+  },
+);
+
+// Delete Evaluation
+export const deleteEvaluation = createAsyncThunk<
+  { notification: INotification; evaluationId: number },
+  { evaluationId: number },
+  { rejectValue: { notification: INotification } }
+>('evaluation/deleteEvaluation', async ({ evaluationId }, { rejectWithValue }) => {
+  try {
+    await axios.delete(`${process.env.SERVER_URL}/api/evaluation/${evaluationId}`, {
+      withCredentials: true,
+    });
+    return {
+      evaluationId,
+      notification: { type: 'success', message: 'Evaluation successfully deleted' },
+    };
+  } catch (e) {
+    const error = e as AxiosError<{ message: string }>;
+    return rejectWithValue({
+      notification: {
+        type: 'error',
+        message: error.response?.data.message || 'Failed to delete evaluation',
+      },
+    });
+  }
+});
 const evaluationSlice = createSlice({
   name: 'evaluationSlice',
   initialState,
-  reducers: {},
-  extraReducers: (builder) => builder,
+  reducers: {
+    setEvaluations: (state, action: PayloadAction<{ evaluations: IEvaluation[] }>) => {
+      state.evaluations = action.payload.evaluations;
+    },
+  },
+  extraReducers: (builder) =>
+    builder
+      // Add Evaluation
+      .addCase(createEvaluation.fulfilled, (state, action) => {
+        state.evaluations.push(action.payload.evaluation);
+      })
+      // .addCase(createEvaluation.rejected, (state) => {})
+      // Update Evaluation
+      .addCase(updateEvaluation.fulfilled, (state, action) => {
+        const { evaluation } = action.payload;
+        const evaluationIndex = state.evaluations.findIndex((e) => e.id === evaluation.id);
+        state.evaluations[evaluationIndex!] = evaluation;
+      })
+      // .addCase(updateEvaluation.rejected, (state) => {})
+      // Delete Evaluation
+      .addCase(deleteEvaluation.fulfilled, (state, action) => {
+        const evaluationIndex = state.evaluations.findIndex(
+          (e) => e.id === action.payload.evaluationId,
+        );
+        state.evaluations.splice(evaluationIndex, 1);
+      }),
+  // .addCase(deleteEvaluation.rejected, (state) => {}),
 });
 
 export const evaluationReducer = evaluationSlice.reducer;
-export const {} = evaluationSlice.actions;
+export const { setEvaluations } = evaluationSlice.actions;
