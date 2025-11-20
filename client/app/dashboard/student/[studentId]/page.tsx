dd4257b50b294b1ddc9436a2f3b5f93884da62e7
Andre Barros <andrebarros7201@gmail.com>
2025-11-20 16:26:48 +0000

    feat(student/studentId): create student page with boilerplate

---
+++ client/app/dashboard/student/[studentId]/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Page } from '@/components/page/Page';
+import { fetchCurrentStudent } from '@/redux/slices/currentStudentSlice';
+import { RootDispatch, RootState } from '@/redux/store';
+import { use, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+type Props = {
+  params: Promise<{ studentId: string }>;
+};
+
+export default function StudentPage({ params }: Props) {
+  const { currentStudent, isLoading } = useSelector((state: RootState) => state.currentStudent);
+  const dispatch = useDispatch<RootDispatch>();
+  const { studentId } = use(params);
+
+  useEffect(() => {
+    dispatch(fetchCurrentStudent({ studentId: Number(studentId) }));
+  }, [dispatch, studentId]);
+
+  if (!isLoading && currentStudent) {
+    return (
+      <Page>
+        <h2>{currentStudent.name}</h2>
+      </Page>
+    );
+  }
+}
