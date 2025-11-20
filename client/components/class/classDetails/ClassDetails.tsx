8233aa808f974db630b87272c90eccba0d912c63
Andre Barros <andrebarros7201@gmail.com>
2025-11-20 21:02:37 +0000

    fix(ClassDetails): remove Props and use currentClass from state for rendering

--- client/components/class/classDetails/ClassDetails.tsx
+++ client/components/class/classDetails/ClassDetails.tsx
@@ -1,37 +1,36 @@
 'use client';
 import classes from './classDetails.module.scss';
 import { ButtonLink } from '@/components/ui/buttonLink/ButtonLink';
-import { IClassComplete } from '@/types/interfaces/IClassComplete';
 import { UpdateUserButton } from '@/components/admin/userButton/updateUserButton/UpdateUserButton';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
 import { Role } from '@/types/enums/RoleEnum';
 import { DeleteUserButton } from '@/components/admin/userButton/deleteUserButton/DeleteUserButton';
 
-type Props = {
-  item: IClassComplete;
-};
-
-export const ClassDetails = ({ item }: Props) => {
+export const ClassDetails = () => {
   const { user } = useSelector((state: RootState) => state.user);
+  const { currentClass } = useSelector((state: RootState) => state.class);
+
   return (
     <main className={classes.classDetails}>
       <div className={classes['classDetails__button']}>
         <ButtonLink label={'Go Back'} href={'/dashboard/class'} />
-        {user?.role === Role.Admin && <UpdateUserButton type={'class'} item={item} />}
-        {user?.role === Role.Admin && <DeleteUserButton type={'class'} item={item} />}
+        {user?.role === Role.Admin && <UpdateUserButton type={'class'} item={currentClass} />}
+        {user?.role === Role.Admin && <DeleteUserButton type={'class'} item={currentClass} />}
       </div>
       <div className={classes['classDetails__field']}>
         <p className={classes['classDetails__label']}>Name</p>
-        <p className={classes['classDetails__value']}>{item.name}</p>
+        <p className={classes['classDetails__value']}>{currentClass!.name}</p>
       </div>
       <div className={classes['classDetails__field']}>
         <p className={classes['classDetails__label']}>School Year</p>
-        <p className={classes['classDetails__value']}>{item.schoolYear}</p>
+        <p className={classes['classDetails__value']}>{currentClass!.schoolYear}</p>
       </div>
       <div className={classes['classDetails__field']}>
         <p className={classes['classDetails__label']}>Is Active</p>
-        <p className={classes['classDetails__value']}>{item.isActive === true ? 'Yes' : 'No'}</p>
+        <p className={classes['classDetails__value']}>
+          {currentClass!.isActive === true ? 'Yes' : 'No'}
+        </p>
       </div>
     </main>
   );
