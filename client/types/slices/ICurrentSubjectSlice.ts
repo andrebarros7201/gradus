5dbf111f19c32084dce79503f7a7fe99aeb18374
Andre Barros <andrebarros7201@gmail.com>
2025-11-20 19:54:49 +0000

    feat(ICurrentSubjectSlice): add subjectList property

--- client/types/slices/ICurrentSubjectSlice.ts
+++ client/types/slices/ICurrentSubjectSlice.ts
@@ -1,6 +1,8 @@
-import { ISubjectComplete } from "../interfaces/ISubjectComplete";
+import { ISubjectComplete } from '../interfaces/ISubjectComplete';
+import { ISubjectSimple } from '../interfaces/ISubjectSimple';
 
 export interface ICurrentSubjectSlice {
   currentSubject: ISubjectComplete | null;
+  subjectList: ISubjectSimple[];
   isLoading: boolean;
 }
