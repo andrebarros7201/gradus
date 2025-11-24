81e379889cddce825d6ea0d0fcc2b7583d4e24b1
Andre Barros <andrebarros7201@gmail.com>
2025-11-24 14:11:07 +0000

    feat(StudentSimpleDto): add ClassName property with JSON ignore condition

--- server/DTOs/Student/StudentSimpleDto.cs
+++ server/DTOs/Student/StudentSimpleDto.cs
@@ -1,6 +1,12 @@
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+
 namespace Server.DTOs.Student;
 
 public class StudentSimpleDto {
     public int Id { get; set; }
     public string Name { get; set; } = String.Empty;
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public string ClassName = String.Empty;
 }
