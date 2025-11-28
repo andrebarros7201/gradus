105c9fd6b2e3db24b9dae9046e8d7af7afee6de8
Andre Barros <andrebarros7201@gmail.com>
2025-11-28 19:24:51 +0000

    remove(/class): remove fetching classes from page

--- client/app/dashboard/class/page.tsx
+++ client/app/dashboard/class/page.tsx
@@ -2,22 +2,12 @@
 
 import { ClassList } from '@/components/classList/ClassList';
 import { Page } from '@/components/page/Page';
-import { fetchAllClasses } from '@/redux/slices/classSlice';
-import { RootDispatch } from '@/redux/store';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 
 export default function ClassPage() {
-  const dispatch = useDispatch<RootDispatch>();
-
-  useEffect(() => {
-    dispatch(fetchAllClasses());
-  }, [dispatch]);
-
   return (
     <Page>
       <h3>Class Management</h3>
-      <ClassList/>
+      <ClassList />
     </Page>
   );
 }
