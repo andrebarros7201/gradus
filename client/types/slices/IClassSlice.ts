8c74672a02937408152b2da3b2339b30326cb4c4
Andre Barros <andrebarros7201@gmail.com>
2025-11-28 19:24:16 +0000

    feat(IClassSlice): add pages to class slice interface

--- client/types/slices/IClassSlice.ts
+++ client/types/slices/IClassSlice.ts
@@ -4,5 +4,6 @@ import { IClassSimple } from '../interfaces/IClassSimple';
 export interface IClassSlice {
   isLoading: boolean;
   classes: IClassSimple[];
+  pages: number | null;
   currentClass: IClassComplete | null;
 }
